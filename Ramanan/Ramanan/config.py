01eef358abd1d668f8ccd0e180ec2e4d2d70a347
a4aneeshc <a4aneeshc@gmail.com>
2017-10-14 01:28:13 +0530

    codes added

---
+++ Ramanan/Ramanan/config.py
@@ -0,0 +1,25 @@
+'''
+Created on Apr 24, 2017
+
+@author: aneesh.c
+'''
+
+# -*- coding: utf-8 -*-
+import sys
+# sys.setdefaultencoding() does not exist, here!
+
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+CLIENT_ACCESS_TOKEN = '0849de02b871445f871cff385571d477'
+
+
+reply_something_else = ['OK, please tell me in your own words \
+what you want to use firearm for']
+
+welcome_note = """ഞാൻ ചപ്പാത്തി കഴിക്കാറില്ല. ചോറ് ആണ് തിന്നണത്.. അതോണ്ട് .. എനിക്ക് ഇംഗ്ലീഷ് , \
+ഹിന്ദി അറിയാനും പാടില്ല .. മലയാളം ഹേ..."""
+
+top_level_buttons = ['Loan Eligibility Criteria', 'Forgot my pin number', '\
+IFSC code', 'Something else']
